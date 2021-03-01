fe7550a21b717ef63503f98ae59b7c57592af78d
consoleLogIt <kishansingh4do@gmail.com>
2021-03-01 11:57:39 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import useLoadSuggetions from "./useLoadSuggetions";
 
 function App() {
+  const [tags, setTags] = useState([]);
+  const [query, setQuery] = useState("");
+  const suggetions = useLoadSuggetions(query);
+
+  const handleAddTags = (e, item) => {
+    e.preventDefault();
+    setTags((prevState) => [item, ...prevState]);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key !== "Enter") return;
+    handleAddTags(e, e.target.value);
+    e.target.value = "";
+    setQuery("");
+  };
+
+  const handleRemoveTag = (e, index) => {
+    e.preventDefault();
+    const modified = tags.filter((item, i) => i !== index);
+    setTags(modified);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="OuterContainer">
+      <div className="innerContainer">
+        <div className="input-tag-container">
+          {tags.length > 0 && (
+            <ul className="tag-list">
+              {tags.map((item, index) => (
+                <li key={index} className="tags">
+                  <span className="tag-name">{item}</span>
+                  <span className="close-btn">
+                    <span
+                      className="cross-icon"
+                      onMouseDown={(e) => handleRemoveTag(e, index)}
+                    ></span>
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
+          <input
+            onKeyPress={handleKeyPress}
+            onChange={(e) => setQuery(e.target.value)}
+            type="text"
+            placeholder="+ Add Tags"
+          ></input>
+        </div>
+        <div className="suggetions-container">
+          {suggetions.length > 0 && (
+            <ul className="suggetions-list">
+              {suggetions.map((item, index) => (
+                <li key={index} onMouseDown={(e) => handleAddTags(e, item)}>
+                  {item}
+                  <sup style={{ padding: "5px" }}>23</sup>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/useLoadSuggetions.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const db = [
+  "Child malnutrition",
+  "Child media management",
+  "Child consultant",
+  "Children",
+  "Pressing Social",
+  "Livestock Management",
+  "Poultry and bird cultivation",
+  "Water Quality Network",
+  "Revitalising Rainfed Agriculture Network",
+];
+
+function useLoadSuggetions(query) {
+  const [suggetions, setSuggetions] = useState([]);
+
+  useEffect(() => {
+   
+    if (query === "") {
+      setSuggetions([]);
+      return;
+    }
+    const modified = db.filter((item) =>
+      item.toLocaleLowerCase().startsWith(query.toLocaleLowerCase())
+    );
+    setSuggetions(modified);
+  }, [query]);
+
+  return suggetions;
+}
+
+export default useLoadSuggetions;
